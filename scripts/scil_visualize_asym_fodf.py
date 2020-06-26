c0db762b2eda99899b30fce7af0ceb6bf4ab8652
CHrlS98 <poic2312@dinf-0051-23b.DInf.fsci.usherbrooke.ca>
2020-06-26 16:05:13 -0400

    Visualization of HCP dataset FODF + error map

--- scripts/scil_visualize_asym_fodf.py
+++ scripts/scil_visualize_asym_fodf.py
@@ -17,25 +17,7 @@ from fury import window, actor
 from scilpy.io.utils import (add_overwrite_arg,
                              add_sh_basis_args)
 
-WINDOW_SIZE=(1024, 1024)
-
-def visualize_sfs(sfs, sphere, scale=0.8, out_path=None, window_size=WINDOW_SIZE):
-    odfs = actor.odf_slicer(sfs, radial_scale=True,
-                            sphere=sphere, colormap='jet', scale=scale)
-    dims = sfs.shape
-    odfs.display_extent(0, dims[0], 0, dims[1], 0, dims[2])
-
-    scene = window.Scene()
-    showm = window.ShowManager(scene, size=window_size, title='Visualize SH')
-    showm.initialize()
-
-    scene.add(odfs)
-
-    showm.start()
-
-    if out_path:
-        window.record(scene, size=WINDOW_SIZE, out_path=out_path, reset_camera=False)
-
+WINDOW_SIZE=(768, 768)
 
 def _build_arg_parser():
     p = argparse.ArgumentParser(description=__doc__,
@@ -49,6 +31,9 @@ def _build_arg_parser():
 
     p.add_argument('slice_index', type=int,
                    help='The index of the slice to visualize in axis_name')
+
+    p.add_argument('--output',
+                   help='Path to output file to write')
 
     p.add_argument('--axis_name', default='axial',
                    help='Name of the axis to visualize.\
@@ -70,6 +55,80 @@ def get_translation_matrix(translation):
                      [0.0, 0.0, 0.0, 1.0]])
 
 
+def display_scene(odf_data, sphere, error_data, shape3D,
+                  orientation, slice_index,output):
+    odf_actor = actor.odf_slicer(odf_data,
+                                 radial_scale=True,
+                                 sphere=sphere,
+                                 colormap='jet',
+                                 scale=0.5)
+    if orientation == 'sagittal':
+        affine_transform = get_translation_matrix([1.0 - slice_index, 0.0, 0.0])
+    elif orientation == 'coronal':
+        affine_transform = get_translation_matrix([0.0, -1.0 - slice_index, 0.0])
+    elif orientation == 'axial':
+        affine_transform = get_translation_matrix([0.0, 0.0, -1.0 - slice_index])
+    else:
+        print('Invalid orientation')
+        return -1
+
+    error_actor = actor.slicer(error_data,
+                               affine=affine_transform,
+                               interpolation='nearest')
+
+    scene = window.Scene()
+    scene.projection('parallel')
+
+    scene.add(odf_actor)
+    scene.add(error_actor)
+    if orientation == 'sagittal':
+        error_actor.display_extent(slice_index, slice_index, 0, shape3D[1] - 1, 0, shape3D[2] - 1)
+        odf_actor.display_extent(0, 0, 0, shape3D[1] - 1, 0, shape3D[2] - 1)
+        view_position = [-280.0,
+                         (shape3D[1] - 1) / 2.0,
+                         (shape3D[2] - 1) / 2.0]
+        view_center = [0.0,
+                       (shape3D[1] - 1) / 2.0,
+                       (shape3D[2] - 1) / 2.0]
+        view_up = [0.0, 0.0, 1.0]
+    elif orientation == 'coronal':
+        error_actor.display_extent(0, shape3D[0] - 1, slice_index, slice_index, 0, shape3D[2] - 1)
+        odf_actor.display_extent(0, shape3D[0] - 1, 0, 0, 0, shape3D[2] - 1)
+        view_position = [(shape3D[0] - 1) / 2.0,
+                         280.0,
+                         (shape3D[2] - 1) / 2.0]
+        view_center = [(shape3D[0] - 1) / 2.0,
+                       0.0,
+                       (shape3D[2] - 1) / 2.0]
+        view_up = [0.0, 0.0, 1.0]
+    elif orientation == 'axial':
+        error_actor.display_extent(0, shape3D[0] - 1, 0, shape3D[1] - 1, slice_index, slice_index)
+        odf_actor.display_extent(0, shape3D[0] - 1, 0, shape3D[1] - 1, 0, 0)
+        view_position = [(shape3D[0] - 1) / 2.0,
+                         (shape3D[1] - 1) / 2.0,
+                         280.0]
+        view_center = [(shape3D[0] - 1) / 2.0,
+                         (shape3D[1] - 1) / 2.0,
+                         0.0]
+        view_up = [0.0, 1.0, 0.0]
+    else:
+        print('Invalid orientation')
+        return -1
+
+    scene.set_camera(position=view_position,
+                     focal_point=view_center,
+                     view_up=view_up)
+    showm = window.ShowManager(scene, size=WINDOW_SIZE,
+                               title='Visualize SH',
+                               reset_camera=False,
+                               interactor_style='image')
+    showm.initialize()
+    showm.start()
+
+    if output:
+        window.record(scene, size=WINDOW_SIZE, out_path=output, reset_camera=False)
+
+
 def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
@@ -80,41 +139,29 @@ def main():
     error_img = nib.nifti1.load(args.input_error)
     error_data = error_img.get_fdata()
 
-    data_slice = None
+    idx = args.slice_index
     if args.axis_name == 'sagittal':
-        fodf_data_slice = fodf_data[args.slice_index:args.slice_index + 1, :, :, :]
-        error_data_slice = error_data[args.slice_index:args.slice_index + 1]
+        fodf_cropped_data = fodf_data[idx:idx+1, :, :, :]
     elif args.axis_name == 'coronal':
-        fodf_data_slice = fodf_data[:, args.slice_index:args.slice_index + 1, :, :]
-        error_data_slice = error_data[:, args.slice_index:args.slice_index + 1]
+        fodf_cropped_data = fodf_data[:, idx:idx+1, :, :]
+    elif args.axis_name == 'axial':
+        fodf_cropped_data = fodf_data[:, :, idx:idx+1, :]
     else:
-        fodf_data_slice = fodf_data[:, :, args.slice_index:args.slice_index + 1, :]
-        error_data_slice = error_data[:, :, args.slice_index:args.slice_index + 1]
+        print('Invalid axis name')
+        return -1
 
     sph_gtab = get_sphere(args.sphere)
 
-    sfs = sh_to_sf(fodf_data_slice, sph_gtab, sh_order=args.sh_order, basis_type='descoteaux07_full')
+    odf_data_sf = sh_to_sf(fodf_cropped_data,
+                                 sph_gtab,
+                                 sh_order=args.sh_order,
+                                 basis_type='descoteaux07_full')
 
-    odfs = actor.odf_slicer(sfs, radial_scale=True, sphere=sph_gtab, colormap='jet', scale=0.5)
-    odfs.display_extent(0, sfs.shape[0], 0, sfs.shape[1], 0, sfs.shape[2])
-
-    error = actor.slicer(error_data_slice,
-                         affine=get_translation_matrix([0.0, 0.0, -1.0]),
-                         interpolation='nearest')
-    #error.display_extent(0, error_data_slice.shape[0],
-    #                     0, error_data_slice.shape[1],
-    #                     0, error_data_slice.shape[2])
-
-    scene = window.Scene()
-    showm = window.ShowManager(scene, size=WINDOW_SIZE, title='Visualize SH')
-    showm.initialize()
-
-    #scene.add(odfs)
-    scene.add(error)
-
-    showm.start()
-
-    window.record(scene, size=WINDOW_SIZE, out_path='hcp_reconst_error.png', reset_camera=False)
+    shape = error_data.shape[:3]
+    display_scene(odf_data_sf, sph_gtab,
+                  error_data, error_data.shape[:3],
+                  args.axis_name, args.slice_index,
+                  args.output)
 
 
 if __name__ == '__main__':
