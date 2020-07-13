d3a331d10a5a45a4a9f4ecb57cb37fe159bdd5bd
charles.poirier <charles.poirier@usherbrooke.ca>
2020-07-13 16:13:02 -0400

    FOD class and better workflow

--- scilpy/reconst/asym_fodf.py
+++ scilpy/reconst/asym_fodf.py
@@ -2,215 +2,219 @@
 
 import logging
 import numpy as np
+import nibabel as nib
 
-from dipy.reconst.shm import sh_to_sf, sf_to_sh
-
-
-def prepare_batch(batch_size, augm_dim):
-    """
-    Compute number of batches and resulting batch size.
-    Resulting batch size will be modified if there is
-    not more than one batch to process
-    """
-    # Last batch can be bigger than the others
-    number_of_batches = int((augm_dim[0] - 2) / (batch_size - 2))
-
-    # Corner case: When the dimension of the
-    # data is smaller than the batch size
-    if number_of_batches == 0:
-        number_of_batches = 1
-        batch_size = augm_dim[0]
-
-    return number_of_batches, batch_size
+from dipy.reconst.shm import (sh_to_sf, sf_to_sh,
+                              sph_harm_full_ind_list)
 
 
-def get_weights_table(sphere, sharpness):
-    """
-    Calculate the dot product (cos(theta)) between vertices
-    on the sphere and directions to adjacent voxels
+class FiberOrientationDistribution(object):
+    def __init__(self, fodf, affine, sh_basis, sh_order):
+        """
+        Build the FiberOrientationDistribution object
 
-    Returns dictionary mapping voxel directions to dot products
-    with sphere vertices in this direction for each direction
-    """
-    # center directions around current voxel
-    directions = np.transpose(np.indices((3, 3, 3)) - np.ones((3, 3, 3)))
-    directions = np.reshape(directions, (27, 3))
-
-    # remove direction (0, 0, 0)
-    directions = np.delete(directions, 13, 0)
-
-    dir_norm = directions / np.linalg.norm(directions, axis=1, keepdims=True)
-    hemispheres = np.dot(sphere.vertices, dir_norm.T)
-    hemispheres = np.where(hemispheres > 0.0, hemispheres**sharpness, 0.0)
-
-    keys = list(map(tuple, directions))
-    hemis_by_dir = dict(zip(keys, list(hemispheres.T)))
-    sum_of_weights = np.sum(hemispheres, axis=-1)
-
-    return hemis_by_dir, sum_of_weights
+        Parameters
+        ==========
+        fodf: numpy array
+            array containing the fodf
+        affine: numpy array
+            voxel to world 4x4 transformation matrix
+        sh_basis: str
+            SH basis of the fodf data. One of 'descoteaux07',
+            'descoteaux07_full', 'tournier07', 'tournier07_full'
+        sh_order: int
+            The maximum order of the SH basis
+        """
+        self.fodf = fodf
+        self.affine = affine
+        self.sh_basis = sh_basis
+        self.sh_order = sh_order
 
 
-def compute_avg_fodf_no_weight(data, sphere, sh_order=8, batch_size=10,
-                           input_sh_basis='descoteaux07'):
-    """
-    Compute the average of fodf in data with
-    its 26 neighbors by batches
-    """
-    # Convert to spherical function
-    sf = np.array([sh_to_sf(i, sphere, sh_order, input_sh_basis) for i in data],
-                  dtype='float32')
+    def average(self, sphere, sh_order=8, sh_basis='descoteaux07_full',
+                dot_sharpness=1.0, batch_size=10):
+        """
+        Average the FODF
 
-    # Initialize array for mean SF with current voxel value
-    mean_sf = np.copy(sf)
+        Parameters
+        ==========
+        sphere: Sphere
+            Sphere to use to project the spherical functions
+        sh_order: int
+            Maximum order of the SH series (default: 8)
+        sh_basis: str
+            SH basis of the averaged data. One of {'descoteaux07',
+            'descoteaux07_full', 'tournier07', 'tournier07_full'}
+            (default: 'descoteaux07_full')
+        dot_sharpness: float
+            Exponent of the dot product. When set to 0.0, directions
+            are not weighted by the dot product (default: 1.0)
+        batch_size: int
+            Number of volume slices processed at a same time. The
+            last batch to be processed can be of a size up to
+            (batch_size * 2.0 - 1) (default: 10)
+        """
+        # Convert to spherical function
+        sf = np.array([sh_to_sf(i, sphere, self.sh_order, self.sh_basis)
+                       for i in self.fodf],
+                      dtype='float32')
 
-    # Zero pad sf data
-    pad_width = ((1, 1),(1, 1),(1, 1),(0, 0))
-    sf = np.pad(sf, pad_width, mode='constant', constant_values=0.0)
+        # Initialize array for mean SF with current voxel value
+        mean_sf = np.copy(sf)
 
-    # Prepare batch
-    number_of_batches, batch_size = prepare_batch(batch_size, sf.shape)
-    hemis_by_dir, sum_of_weights = get_weights_table(sphere, 0.0)
+        # Zero pad sf data
+        sf = np.pad(sf, ((1, 1),(1, 1),(1, 1),(0, 0)),
+                    mode='constant', constant_values=0.0)
 
-    # Compute average in batches
-    for num_batch in range(number_of_batches):
-        # Select batch to process
-        start = int(num_batch * (batch_size - 2))
-        stop = int(start + batch_size)
-        if (num_batch + 1) * (batch_size - 2) + batch_size > sf.shape[0]:
-            stop = sf.shape[0]
+        # Prepare batch
+        batch_indices = self._get_batches_indices(batch_size)
+        hemis_by_dir, sum_of_weights =\
+            self._get_dot_weights(sphere, dot_sharpness)
 
-        batch = sf[start:stop]
-        dim = (batch.shape[0] - 2,
-               batch.shape[1] - 2,
-               batch.shape[2] - 2,
-               batch.shape[3])
+        # Compute average in batches
+        for batch_index in batch_indices:
+            batch = sf[batch_index]
+            dim = (batch.shape[0] - 2, 
+                   batch.shape[1] - 2,
+                   batch.shape[2] - 2, 
+                   batch.shape[3])
 
-        for key in hemis_by_dir:
-            i, j, k = int(key[0] + 1), int(key[1] + 1), int(key[2] + 1)
-            hemisphere = hemis_by_dir[key]
-            mean_sf[start:start + dim[0]] += \
-                np.multiply(batch[i:dim[0]+i, j:dim[1]+j, k:dim[2]+k],
-                            hemisphere)
+            for key in hemis_by_dir:
+                direction = np.array([key])
+                hemisphere = hemis_by_dir[key]
+                i, j, k = int(key[0] + 1), int(key[1] + 1), int(key[2] + 1)
+                mean_sf[batch_index[0]:batch_index[0] + dim[0]] += \
+                    np.multiply(batch[i:dim[0]+i, j:dim[1]+j, k:dim[2]+k], 
+                                hemisphere)
 
-        mean_sf[start:start + dim[0]] = \
-            np.multiply(mean_sf[start:start + dim[0]],
-                        1.0 / (sum_of_weights + 1.0))
+            mean_sf[batch_index[0]:batch_index[0] + dim[0]] = \
+                np.multiply(mean_sf[batch_index[0]:batch_index[0] + dim[0]],
+                            1.0 / (sum_of_weights + 1.0))
 
-    mean_sh = np.array([sf_to_sh(i, sphere, sh_order, 'descoteaux07_full') for i in mean_sf])
-
-    # DEBUG
-    #a = np.mean(mean_sf, axis=(0, 1, 2))
-    #b = np.mean(sf[1:-1, 1:-1, 1:-1], axis=(0, 1, 2))
-    #mean_error = np.mean(np.abs(b - a))
-    #print('Mean error: ', mean_error)
-    # DEBUG
-
-    return mean_sh
-
-
-def compute_avg_fodf_with_weights(data, sphere, sh_order=8, sharpness=1.0,
-                              batch_size=10, input_sh_basis='descoteaux07'):
-    """
-    Compute the average of fodf in data with
-    its 26 neighbors by batches
-    """
-    # Convert to spherical function
-    sf = np.array([sh_to_sf(i, sphere, sh_order, input_sh_basis) for i in data],
-                  dtype='float32')
-
-    # Initialize array for mean SF with current voxel value
-    mean_sf = np.copy(sf)
-
-    # Zero pad sf data
-    pad_width = ((1, 1),(1, 1),(1, 1),(0, 0))
-    sf = np.pad(sf, pad_width, mode='constant', constant_values=0.0)
-
-    # Prepare batch
-    number_of_batches, batch_size = prepare_batch(batch_size, sf.shape)
-    hemis_by_dir, sum_of_weights = get_weights_table(sphere, sharpness)
-
-    # Compute average in batches
-    for num_batch in range(number_of_batches):
-        # Select batch to process
-        start = int(num_batch * (batch_size - 2))
-        stop = int(start + batch_size)
-        if (num_batch + 1) * (batch_size - 2) + batch_size > sf.shape[0]:
-            stop = sf.shape[0]
-
-        batch = sf[start:stop]
-        dim = (batch.shape[0] - 2, batch.shape[1] - 2,
-               batch.shape[2] - 2, batch.shape[3])
-
-        for key in hemis_by_dir:
-            direction = np.array([key])
-            hemisphere = hemis_by_dir[key]
-            i, j, k = int(key[0] + 1), int(key[1] + 1), int(key[2] + 1)
-            mean_sf[start:start + dim[0]] += \
-                np.multiply(batch[i:dim[0]+i, j:dim[1]+j, k:dim[2]+k], 
-                            hemisphere)
-
-        mean_sf[start:start + dim[0]] = \
-            np.multiply(mean_sf[start:start + dim[0]],
-                        1.0 / (sum_of_weights + 1.0))
-
-    # DEBUG
-    #a = np.mean(mean_sf, axis=(0, 1, 2))
-    #b = np.mean(sf[1:-1, 1:-1, 1:-1], axis=(0, 1, 2))
-    #mean_error = np.mean(np.abs(b - a))
-    #print('Mean error: ', mean_error)
-    # DEBUG
-
-    mean_sh = np.array([sf_to_sh(i, sphere, sh_order, 'descoteaux07_full') for i in mean_sf])
-
-    return mean_sh
+        self.fodf =\
+            np.array([sf_to_sh(i, sphere, sh_order, sh_basis) 
+                      for i in mean_sf])
+        self.sh_basis = sh_basis
+        self.sh_order = sh_order
 
 
-def compute_diff_fodf(input_data, averaged_data, sh_order, symmetric_basis, full_sh_basis, sphere):
-    """
-    Compute the fodf resulting from the subtraction of the original 
-    symmetric signal to the averaged full signal
-    """
-    input_sf = np.array([sh_to_sf(slice_i, sphere, sh_order, symmetric_basis) for slice_i in input_data])
-    mean_sf = np.array([sh_to_sf(slice_i, sphere, sh_order, full_sh_basis) for slice_i in averaged_data])
+    def save_to_file(self, filename):
+        """
+        Save the FODF to file under given filename
 
-    resulting_sf = mean_sf - input_sf
-    output_sh = np.array([sf_to_sh(slice_i, sphere, sh_order, full_sh_basis) for slice_i in resulting_sf])
-
-    return output_sh
-
-
-def compute_error(input_data, averaged_data, sh_order, symmetric_basis, full_sh_basis, sphere):
-    """
-    Compute mean square error between signal recovered from our symmetric
-    basis and the signal recovered from a full SH basis
-    """
-    input_sf = np.array([sh_to_sf(slice_i, sphere, sh_order, symmetric_basis) for slice_i in input_data])
-    mean_sf = np.array([sh_to_sf(slice_i, sphere, sh_order, full_sh_basis) for slice_i in averaged_data])
-
-    error = np.sum((mean_sf - input_sf)**2, axis=-1)
-    return error / error.max()
+        Parameters
+        ==========
+        filename: str
+            name of the file to save with extension
+        """
+        image = nib.Nifti1Image(self.fodf.astype(np.float32), self.affine)
+        image.to_filename(filename)
 
 
-def compute_reconst_error(averaged_data, sh_order, symmetric_basis, full_sh_basis, sphere):
-    """
-    Compute the reconstruction error when using a symmetric basis to average the signal 
-    obtained from a full spherical harmonics basis
-    """
-    full_sf = np.array([sh_to_sf(i, sphere, sh_order, full_sh_basis) for i in averaged_data])
-    sym_sh = np.array([sf_to_sh(i, sphere, sh_order, symmetric_basis) for i in full_sf])
+    def normalize_by_voxel(self):
+        """
+        Perform voxel-wise normalization of FODF (in place)
+        """
+        norm = np.linalg.norm(self.fodf, axis=-1, keepdims=False)
+        normalized_sh = np.zeros_like(self.fodf)
+        mask = norm > 0
+        masked_norm = np.reshape(norm[mask],
+                                (norm[mask].shape[0], 1))
+        normalized_sh[mask] = self.fodf[mask] / masked_norm
 
-    sym_sf = np.array([sh_to_sf(i, sphere, sh_order, symmetric_basis) for i in sym_sh])
-
-    error = np.sum((full_sf - sym_sf)**2, axis=-1)
-    return error / error.max()
+        self.fodf = normalized_sh
 
 
-def compute_diff_mask(original_data, averaged_data):
-    """
-    Compute a mask showing highlighting the zones where a FODF is only
-    present in one of the images
-    """
-    mask_for_original = (np.sum(np.abs(original_data), axis=-1)) > 0
-    mask_for_averaged = (np.sum(np.abs(averaged_data), axis=-1)) > 0
+    def compute_asymmetry_measure(self):
+        """
+        Measure the asymmetry of the FODF per voxel.
 
-    return (mask_for_averaged != mask_for_original)
+        Note
+        ====
+        The asymmetry measure corresponds to the ratio of the norm of odd
+        order SH coefficients on the norm of full order SH order coefficients
+        """
+        if not '_full' in self.sh_basis:
+            logging.error('Can\'t compute asymmetry measure ' +\
+                          'from symmetric SH basis')
+            import sys
+            sys.exit('Aborting program')
+
+        _, l_list = sph_harm_full_ind_list(self.sh_order)
+        odd_order_coeffs = self.fodf[..., l_list % 2==1]
+        odd_order_norms = np.linalg.norm(odd_order_coeffs, axis=-1)
+        full_norms = np.linalg.norm(self.fodf, axis=-1)
+        
+        asymmetry_measure = np.zeros_like(full_norms)
+        mask = full_norms > 0
+
+        asymmetry_measure[mask] = odd_order_norms[mask] / full_norms[mask]
+        return asymmetry_measure
+
+
+    def _get_batches_indices(self, batch_size):
+        """
+        Get the index of slices of data set along the first axis
+
+        Parameters
+        ==========
+        batch_size: int
+            Number of slices per batch
+        
+        Returns
+        =======
+        split_indices: list of indices for each batch
+            List of indices for each batch
+        """
+        nb_slices = self.fodf.shape[0]
+        pad_width = 2
+
+        number_of_batches = int(nb_slices / batch_size + 1.0)
+        indices = np.arange(number_of_batches * batch_size + pad_width)
+        split_indices = []
+        for batch_id in range(number_of_batches):
+            start = batch_size * batch_id
+            split_indices.append(indices[start:start + batch_size + pad_width])
+
+        split_indices[-1] =\
+            split_indices[-1][split_indices[-1] < nb_slices + pad_width]
+
+        return split_indices
+
+
+    def _get_dot_weights(self, sphere, sharpness):
+        """
+        Calculate the dot product (cos(theta))**sharpness between
+        vertices on the sphere and directions to adjacent voxels
+
+        Parameters
+        ==========
+        sphere: Sphere
+            sphere on which the SF is approximated
+        sharpness: float
+            exponent of the dot product
+
+        Returns
+        =======
+        hemis_by_dir: dictionary
+            dictionary mapping voxel directions to dot products with
+            sphere vertices in this direction for each direction
+        sum_of_weights: numpy array
+            Normalization factor for each averaged direction on the sphere
+        """
+        # center directions around current voxel
+        directions = np.transpose(np.indices((3, 3, 3)) - np.ones((3, 3, 3)))
+        directions = np.reshape(directions, (27, 3))
+
+        # remove direction (0, 0, 0)
+        directions = np.delete(directions, 13, 0)
+
+        dir_norm = directions / np.linalg.norm(directions, axis=1, keepdims=True)
+        hemispheres = np.dot(sphere.vertices, dir_norm.T)
+        hemispheres = np.where(hemispheres > 0.0, hemispheres**sharpness, 0.0)
+
+        keys = list(map(tuple, directions))
+        hemis_by_dir = dict(zip(keys, list(hemispheres.T)))
+        sum_of_weights = np.sum(hemispheres, axis=-1)
+
+        return hemis_by_dir, sum_of_weights
--- scripts/scil_average_fodf.py
+++ scripts/scil_average_fodf.py
@@ -17,8 +17,7 @@ from dipy.data import get_sphere
 from scilpy.io.utils import (add_overwrite_arg, assert_inputs_exist,
                              assert_outputs_exist, add_sh_basis_args)
 
-from scilpy.reconst.asym_fodf import (compute_avg_fodf_with_weights,
-                                      compute_avg_fodf_no_weight)
+from scilpy.reconst.asym_fodf import (FiberOrientationDistribution)
 
 
 def _build_arg_parser():
@@ -28,8 +27,11 @@ def _build_arg_parser():
     p.add_argument('input',
                    help='Path to the input fODF file')
     
-    p.add_argument('output',
-                   help='Output path with extension')
+    p.add_argument('--avfod',
+                   help='Output path of averaged fODF')
+
+    p.add_argument('--asym_measure',
+                   help='Output path of asymmetry measure file')
 
     p.add_argument(
         '--sh_order', metavar='int', default=8, type=int,
@@ -38,11 +40,6 @@ def _build_arg_parser():
     p.add_argument(
         '--sphere', default='symmetric724',
         help='Sphere used for the SH reprojection'
-    )
-
-    p.add_argument(
-        '--weighted', default=False, action='store_true',
-        help='Use weights from dot product in average'
     )
 
     p.add_argument(
@@ -66,12 +63,17 @@ def main():
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
 
+    outputs = []
+    if args.avfod:
+        outputs.append(args.avfod)
+    if args.asym_measure:
+        outputs.append(args.asym_measure)
+    if not outputs:
+        parser.error('No output to be done.')
+
     # Checking args
     assert_inputs_exist(parser, args.input)
-    assert_outputs_exist(parser, args, args.output, check_dir_exists=True)
-
-    if args.batch_size < 3:
-        parser.error('Batch size must be of at least 3.')
+    assert_outputs_exist(parser, args, outputs, check_dir_exists=True)
 
     # Prepare data
     sphere = get_sphere(args.sphere)
@@ -80,27 +82,26 @@ def main():
     img_data = img.get_fdata()
     affine = img.affine
 
+    FOD = FiberOrientationDistribution(img_data,
+                                       affine,
+                                       args.sh_basis,
+                                       args.sh_order)
+
     # Computing neighbors average of fODFs
     t0 = time.perf_counter()
-    avg_img = None
-    if args.weighted:
-        logging.info('Computing average fodf (weighted)')
-        avg_fodf = compute_avg_fodf_with_weights(img_data, sphere, args.sh_order,
-                                             args.sharpness, args.batch_size,
-                                             args.sh_basis)
-        avg_img = nib.Nifti1Image(avg_fodf.astype(np.float32), affine)
-    else:
-        logging.info('Computing average fodf (not weighted)')
-        avg_fodf = compute_avg_fodf_no_weight(img_data, sphere, args.sh_order,
-                                          args.batch_size, args.sh_basis)
-        avg_img = nib.Nifti1Image(avg_fodf.astype(np.float32), affine)
+    if args.avfod:
+        FOD.average(sphere, dot_sharpness=args.sharpness,
+                    batch_size=args.batch_size)
+        FOD.save_to_file(args.avfod)
+    if args.asym_measure:
+        asym_measure = FOD.compute_asymmetry_measure()
+        asym_measure_img = \
+            nib.Nifti1Image(asym_measure.astype(np.float32), affine)
+        asym_measure_img.to_filename(args.asym_measure)
     t1 = time.perf_counter()
 
     elapsedTime = t1 - t0
     print('Elapsed time (s): ', elapsedTime)
-
-    logging.info('Saving output')
-    avg_img.to_filename(args.output)
 
 
 if __name__ == "__main__":
