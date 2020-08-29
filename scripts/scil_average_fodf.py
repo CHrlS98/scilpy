df0171282b8b90b47170a4e6550907f0f3a993a2
charles.poirier <charles.poirier@usherbrooke.ca>
2020-08-29 14:08:56 -0400

    Add average-fodf files from branch asym_experiments

---
+++ scripts/scil_average_fodf.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Script to compute neighbors average from fODF
+"""
+
+import time
+import argparse
+import logging
+
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dipy.data import get_sphere
+from dipy.direction.peaks import reshape_peaks_for_visualization
+
+from scilpy.io.utils import (add_overwrite_arg, assert_inputs_exist,
+                             assert_outputs_exist, add_sh_basis_args)
+
+from scilpy.reconst.asym_utils import (AFiberOrientationDistribution,
+                                       APeaks)
+
+
+def _build_arg_parser():
+    p = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+
+    p.add_argument('input',
+                   help='Path to the input file')
+
+    p.add_argument('--avfodf',
+                   help='Output path of averaged fODF')
+
+    p.add_argument('--rm_false_pos',
+                   help='Output path of cleaned fODF file.')
+
+    p.add_argument('--peaks',
+                   help='Output path of peak directions file')
+
+    p.add_argument('--nupeaks',
+                   help='Output path of NuPeaks file')
+
+    p.add_argument('--out_mask',
+                   help='Out path of FODF mask')
+
+    p.add_argument('--epsilon', default=1e-16, type=float,
+                   help='Float epsilon for removing false positives '
+                   '[%(default)s]')
+
+    p.add_argument(
+        '--sh_order', metavar='int', default=8, type=int,
+        help='SH order of the input [%(default)s]')
+
+    p.add_argument(
+        '--sphere', default='symmetric724',
+        help='Sphere used for the SH reprojection [%(default)s]'
+    )
+
+    p.add_argument(
+        '--sharpness', default=1.0, type=float,
+        help='Specify sharpness factor to use for weighted average'
+        ' [%(default)s]'
+    )
+
+    p.add_argument(
+        '--batch_size', default=10, type=int,
+        help='Size of batches when computing average [%(default)s]'
+    )
+
+    p.add_argument(
+        '--sigma', default=1.0, type=float,
+        help='Sigma of the gaussian to use [%(default)s]'
+    )
+
+    p.add_argument(
+        '--mask_off', default=False, action='store_true',
+        help='Mask null fodf [%(default)s]'
+    )
+
+    p.add_argument(
+        '--npeaks', default=10, type=int,
+        help='Number of peaks for peak extraction [%(default)s]'
+    )
+
+    p.add_argument(
+        '--a_threshold', default=0.0, type=float,
+        help='Absolute threshold for peak extraction [%(default)s]'
+    )
+
+    p.add_argument(
+        '--r_threshold', default=0.3, type=float,
+        help='Relative threshold for peak extraction [%(default)s]'
+    )
+
+    add_sh_basis_args(p)
+    add_overwrite_arg(p)
+
+    return p
+
+
+def main():
+    parser = _build_arg_parser()
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+
+    outputs = []
+    if args.avfodf:
+        outputs.append(args.avfodf)
+    if args.rm_false_pos:
+        outputs.append(args.rm_false_pos)
+    if args.peaks:
+        outputs.append(args.peaks)
+    if args.nupeaks:
+        outputs.append(args.nupeaks)
+    if args.out_mask:
+        outputs.append(args.out_mask)
+    if not outputs:
+        parser.error('No output to be done.')
+
+    # Checking args
+    assert_inputs_exist(parser, args.input)
+    assert_outputs_exist(parser, args, outputs, check_dir_exists=True)
+
+    # Prepare data
+    sphere = get_sphere(args.sphere)
+    img = nib.nifti1.load(args.input)
+
+    img_data = img.get_fdata()
+    affine = img.affine
+
+    FOD = AFiberOrientationDistribution(img_data,
+                                        affine,
+                                        args.sh_basis,
+                                        args.sh_order)
+
+    # Computing neighbors average of fODFs
+    t0 = time.perf_counter()
+    if args.rm_false_pos:
+        logging.info('Cleaning FODF')
+        FOD.clean_false_pos(args.epsilon)
+        FOD.save_to_file(args.rm_false_pos)
+    if args.out_mask:
+        nib.save(nib.Nifti1Image(FOD.get_mask().astype(np.uint8), affine),
+                 args.out_mask)
+    if args.avfodf:
+        logging.info('Average FODF')
+        FOD.average(sphere, dot_sharpness=args.sharpness, sigma=args.sigma,
+                    batch_size=args.batch_size, mask=(not args.mask_off))
+        FOD.save_to_file(args.avfodf)
+    if args.peaks:
+        logging.info('Extract peaks')
+        peaks = FOD.extract_peaks(sphere, args.npeaks,
+                                  a_threshold=args.a_threshold,
+                                  r_threshold=args.r_threshold)
+        peaks.save_to_file(args.peaks)
+    if args.nupeaks:
+        if not args.peaks:
+            peaks = FOD.extract_peaks(sphere, args.npeaks,
+                                      a_threshold=args.a_threshold,
+                                      r_threshold=args.r_threshold)
+        peaks.save_nupeaks(args.nupeaks)
+    t1 = time.perf_counter()
+
+    elapsedTime = t1 - t0
+    print('Elapsed time (s): ', elapsedTime)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/scil_compute_avfodf_metrics.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Script to compute neighbors average from fODF
+"""
+
+import time
+import argparse
+import logging
+
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dipy.data import get_sphere
+from dipy.direction.peaks import reshape_peaks_for_visualization
+
+from scilpy.io.utils import (add_overwrite_arg, assert_inputs_exist,
+                             assert_outputs_exist, add_sh_basis_args)
+
+from scilpy.reconst.asym_utils import (AFiberOrientationDistribution,
+                                       APeaks, AFODMetricsPopper)
+
+
+def _build_arg_parser():
+    p = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+
+    p.add_argument('--in_fodf',
+                   help='Path to the input FODF file')
+
+    p.add_argument('--in_peaks',
+                   help='Path to the input peaks file')
+
+    p.add_argument('--in_nupeaks',
+                   help='Path to the input NuPeaks file')
+
+    p.add_argument('--in_ofr',
+                   help='Path to the input OFR file')
+
+    p.add_argument('--in_vf',
+                   help='Path to input volume fractions file')
+
+    p.add_argument('--out_ofr',
+                   help='Output path of odd on full coefficients ratio file')
+
+    p.add_argument('--out_labels',
+                   help='Output path of the labeled image using ' +
+                        'OFR asymmetry measure')
+
+    p.add_argument('--out_crossings',
+                   help='Output path of crossing ratios file')
+
+    p.add_argument('--out_proportions',
+                   help='Output path of the NuPeaks proportions file')
+
+    p.add_argument('--wm_th', default=0.30, type=float,
+                   help='WM threshold in volume fraction map for WM mask'
+                   ' [%(default)s]')
+
+    p.add_argument(
+        '--sh_order', metavar='int', default=8, type=int,
+        help='SH order of the input (Default: [%(default)s])')
+
+    add_sh_basis_args(p)
+    add_overwrite_arg(p)
+
+    return p
+
+
+def get_inputs_list(args, parser):
+    inputs = []
+    if args.in_fodf:
+        inputs.append(args.in_fodf)
+    if args.in_peaks:
+        inputs.append(args.in_peaks)
+        if args.in_nupeaks:
+            inputs.append(args.in_nupeaks)
+    if args.in_ofr:
+        inputs.append(args.in_ofr)
+    if args.in_vf:
+        inputs.append(args.in_vf)
+    if not inputs:
+        parser.error('No input: Please supply at least one input')
+    return inputs
+
+
+def get_outputs_list(args, parser):
+    outputs = []
+    if args.out_ofr:
+        if not args.in_fodf:
+            parser.error('Can\'t compute odd/full coefficients\
+                          ratio without FODF file')
+        outputs.append(args.out_ofr)
+    if args.out_labels:
+        if not args.in_peaks:
+            parser.error('Can\'t produce labels without peaks file')
+        outputs.append(args.out_labels)
+    if args.out_crossings:
+        if not args.in_peaks:
+            parser.error('Can\'t compute ratio of crossing'
+                         ' fibers without peaks file')
+        if not args.in_vf:
+            parser.error('Can\'t compute proportion of crossings without'
+                         ' volume fractions')
+        outputs.append(args.out_crossings)
+    if args.out_proportions:
+        if not args.in_peaks:
+            parser.error('Can\'t compute proportions without peaks file')
+        if not args.in_vf:
+            parser.error('Can\'t compute proportions without volume fraction')
+        outputs.append(args.out_proportions)
+    if not outputs:
+        parser.error('No output to be done.')
+    return outputs
+
+
+def write_to_file(filename, content):
+    file = open(filename, 'w')
+    file.write(content)
+    file.close()
+
+
+def load_inputs(args):
+    FOD = None
+    peaks = None
+    ofr = None
+    vf = None
+    if args.in_fodf:
+        fodf_img = nib.nifti1.load(args.in_fodf)
+        fodf_data = fodf_img.get_fdata()
+        fodf_affine = fodf_img.affine
+        FOD = AFiberOrientationDistribution(fodf_data,
+                                            fodf_affine,
+                                            args.sh_basis,
+                                            args.sh_order)
+    if args.in_peaks:
+        peaks_img = nib.nifti1.load(args.in_peaks)
+        peaks_data = peaks_img.get_fdata()
+        peaks_affine = peaks_img.affine
+        nupeaks =\
+            nib.nifti1.load(args.in_nupeaks).get_fdata().astype(np.uint8)\
+            if args.in_nupeaks else None
+        peaks = APeaks(peaks_data, peaks_affine, nupeaks)
+    if args.in_ofr:
+        ofr = nib.nifti1.load(args.in_ofr).get_fdata()
+    if args.in_vf:
+        vf = nib.nifti1.load(args.in_vf).get_fdata()
+    return FOD, peaks, ofr, vf
+
+
+def main():
+    parser = _build_arg_parser()
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+
+    # Checking args
+    inputs = get_inputs_list(args, parser)
+    outputs = get_outputs_list(args, parser)
+    assert_inputs_exist(parser, inputs)
+    assert_outputs_exist(parser, args, outputs, check_dir_exists=True)
+
+    FOD, peaks, ofr, vf = load_inputs(args)
+    metrics_popper = AFODMetricsPopper(FOD, peaks, ofr, vf)
+
+    # Computing AVFODF metrics
+    t0 = time.perf_counter()
+    if args.out_ofr:
+        logging.info('Compute odd/full coefficients ratio')
+        metrics_popper.compute_odd_on_full_coeffs_ratio()
+        metrics_popper.save_odf_on_full_coeffs_ratio(args.out_ofr)
+    if args.out_labels:
+        logging.info('Label intra-voxel configurations using OFR map')
+        metrics_popper.compute_configs_labels_from_ofr()
+        metrics_popper.save_fiber_config_labels(args.out_labels)
+    if args.out_crossings:
+        logging.info('Compute crossings proportion in WM '
+                     'for varying threshold')
+        range_max = vf[..., -1].max()
+        x = np.arange(0.0, range_max, 0.01)
+        y = np.array([metrics_popper.get_crossing_fibers_proportions('wm', th)
+                      for th in x])
+        output = 'WM mask threshold, Proportion of crossings\n'
+        for i in range(x.shape[0]):
+            output += '{0:.3}, {1:.4}\n'.format(x[i], y[i])
+        write_to_file(args.out_crossings, output)
+    if args.out_proportions:
+        logging.info('Compute number of peaks proportions')
+        proportions = metrics_popper.get_nupeaks_proportions('wm', args.wm_th)
+        output = 'NuPeaks, '\
+                 'Proportion of n-peaks FODF inside WM mask (th = {0:.3g})\n'\
+                 .format(args.wm_th)
+        for i in range(proportions.shape[0]):
+            output += '{0}, {1}\n'.format(i, proportions[i])
+        write_to_file(args.out_proportions, output)
+    t1 = time.perf_counter()
+
+    elapsedTime = t1 - t0
+    print('Elapsed time (s): ', elapsedTime)
+
+
+if __name__ == "__main__":
+    main()
