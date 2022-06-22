d308d128bd9defd8b24855218ec23fe91e2a6b35
Charles Poirier <charles.poirier@usherbrooke.ca>
2022-06-22 17:04:15 -0400

    Visualize FTD with ODF overlay

--- scripts/scil_visualize_ftd.py
+++ scripts/scil_visualize_ftd.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 import argparse
+import nibabel as nib
 import numpy as np
 from scilpy.reconst.ftd import FTDFit
 from fury import actor, window
+from dipy.reconst.shm import sh_to_sf_matrix
+from dipy.data import get_sphere
+
+from scilpy.reconst.utils import get_sh_order_and_fullness
 
 
 def _build_arg_parser():
     p = argparse.ArgumentParser()
     p.add_argument('ftd_json')
+    p.add_argument('odf')
 
     return p
 
@@ -22,7 +28,6 @@ def visualize_as_lines(ftd, n_per_edge=2):
             for z in edge_ticks:
                 seeds.append([x, y, z])
     seeds = np.asarray(seeds)
-    print(seeds)
 
 
 def main():
@@ -30,9 +35,11 @@ def main():
     args = parser.parse_args()
 
     ftd = FTDFit.from_json(args.ftd_json)
+    odf_image = nib.load(args.odf)
+    odf = odf_image.get_fdata()
     visualize_as_lines(ftd)
-    1/0
 
+    norm_max = 1.4
     ftd_grid_pos = np.indices((5, 5, 1), dtype=float)
     ftd_grid_pos = ftd_grid_pos.reshape(3, -1).T / 5.0 +\
         np.array([0.0, 0.0, 0.5])
@@ -45,15 +52,29 @@ def main():
         positions = np.asarray(vox_pos, dtype=float) + ftd_grid_pos
         for p in positions:
             for dir in ftd[p]:
-                centers.extend([p]*2)
-                dirs.extend([dir, -dir])
+                if np.linalg.norm(dir) < norm_max:
+                    centers.extend([p, p])
+                    dirs.extend([-dir, dir])
 
     norms = np.linalg.norm(dirs, axis=-1)
+
     colors = np.abs(dirs / norms[..., None])
-    arrows = actor.arrow(centers, dirs, colors=colors, heights=0.2)
+    centers = np.asarray(centers) - 0.5
+    arrows = actor.arrow(centers, dirs, colors=colors,
+                         heights=0.2*norms, tip_length=0.1)
+
+    sphere = get_sphere('symmetric724')
+    order, full_basis = get_sh_order_and_fullness(odf.shape[-1])
+    b_mat = sh_to_sf_matrix(sphere, order,
+                            full_basis=full_basis,
+                            return_inv=False)
+    odf_slicer = actor.odf_slicer(odf, sphere=sphere,
+                                  B_matrix=b_mat, opacity=0.5)
 
     scene = window.Scene()
     scene.add(arrows)
+    scene.add(odf_slicer)
+
     window.show(scene)
 
 
