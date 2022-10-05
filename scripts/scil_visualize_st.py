08e3fc20c3359a5fc286da88d02ac1f81ee112f0
Charles Poirier <charles.poirier@usherbrooke.ca>
2022-10-05 14:41:11 -0400

    Visualise st displays centroids

--- scripts/scil_visualize_st.py
+++ scripts/scil_visualize_st.py
@@ -4,10 +4,12 @@ import json
 import numpy as np
 import nibabel as nib
 from scilpy.io.streamlines import load_tractogram_with_reference
-from scilpy.io.utils import add_reference_arg, snapshot
+from scilpy.io.utils import (add_overwrite_arg, add_reference_arg, snapshot,
+                             assert_inputs_exist, assert_outputs_exist)
 from scilpy.reconst.utils import get_sh_order_and_fullness
 from scilpy.io.image import get_data_as_mask
 
+from dipy.tracking.streamlinespeed import set_number_of_points
 from dipy.reconst.shm import sh_to_sf_matrix
 from dipy.data import get_sphere
 from fury import actor, window, colormap
@@ -23,45 +25,39 @@ def _build_arg_parser():
     p.add_argument('in_fodf')
 
     p.add_argument('--opacity', type=float, default=1.0)
+    p.add_argument('--max_per_cluster', type=int, default=5)
     p.add_argument('--output')
 
     add_reference_arg(p)
+    add_overwrite_arg(p)
     return p
 
 
-def snapshot_voxel(streamlines, vox2tracks, vox2ids, rois_mask,
-                   opacity=None, seeds=None):
-    x, y, z = np.nonzero(rois_mask)
-    actors = []
-    for vox in zip(x, y, z):
-        key = np.array2string(np.asarray(vox))
-        strl_ids = vox2tracks[key]
-        cluster_ids = vox2ids[key]
-        nb_clusters = np.max(cluster_ids) + 1
-        strl = streamlines[strl_ids]
-        colors = colormap.distinguishable_colormap(nb_colors=nb_clusters)
-
-        for cluster_i in range(nb_clusters):
-            c_strl = strl[np.asarray(cluster_ids) == cluster_i]
-            c_opacity = float(len(c_strl)) / float(len(strl))\
-                if opacity is None else opacity
-            color = np.tile(colors[cluster_i], len(c_strl)).reshape(-1, 3)
-            a = actor.line(c_strl, colors=color, opacity=c_opacity)
-            actors.append(a)
-
-        if seeds is not None:
-            seeds_a = actor.dots(seeds[strl_ids], color=(1, 1, 1), opacity=0.8)
-            actors.append(seeds_a)
-
-    s = window.Scene()
-    s.background((1, 1, 1))
-    s.add(*actors)
-    window.show(s)
+def compute_centroid(bundle):
+    bundle = np.asarray([set_number_of_points(s, 20) for s in bundle])
+    centroids = bundle[0]
+    n_lines = 1
+    for i, line in enumerate(bundle[1:]):
+        dist_strt = np.mean(np.sum((centroids/n_lines - line)**2.0,
+                            axis=-1))
+        dist_flip = np.mean(np.sum((centroids/n_lines - line[::-1])**2.0,
+                            axis=-1))
+        if dist_flip < dist_strt:
+            centroids += line[::-1]
+        else:
+            centroids += line
+        n_lines += 1
+    return centroids / n_lines
 
 
 def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
+    assert_inputs_exist(parser, required=[args.in_st, args.in_vox2tracks,
+                                          args.in_vox2ids, args.in_mask,
+                                          args.in_mask, args.in_rois,
+                                          args.in_fodf])
+    assert_outputs_exist(parser, args, [], optional=args.output)
 
     vox2tracks = json.load(open(args.in_vox2tracks, 'r'))
     vox2ids = json.load(open(args.in_vox2ids, 'r'))
@@ -87,36 +83,50 @@ def main():
         cluster_ids = vox2ids[key]
         nb_clusters = np.max(cluster_ids) + 1
         strl = streamlines[strl_ids]
-        colors = colormap.distinguishable_colormap(nb_colors=nb_clusters)
+        outlier_gray = (0.5, 0.5, 0.5)
+        colors = colormap.distinguishable_colormap(nb_colors=nb_clusters,
+                                                   exclude=[outlier_gray])
 
         for cluster_i in range(nb_clusters):
             c_strl = strl[np.asarray(cluster_ids) == cluster_i]
-            color = np.tile(colors[cluster_i], len(c_strl)).reshape(-1, 3)
-            a = actor.line(c_strl, colors=color, linewidth=2.0,
-                           opacity=args.opacity)
+            if len(c_strl) < 2:
+                # clusters containing only one member are outliers!
+                centroid = c_strl
+                color = np.asarray([outlier_gray])
+            else:
+                centroid = compute_centroid(c_strl)[None, ...]
+                color = colors[cluster_i].reshape(-1, 3)
+                txt_a = actor.text_3d('{}'.format(len(c_strl)),
+                                      font_size=0.6, shadow=False,
+                                      position=centroid[0, 0] +
+                                      np.array([0.0, 0.0, 1.0]),
+                                      color=color)
+                actors.append(txt_a)
+            # draw centroids
+            a = actor.line(centroid, colors=color.reshape(-1, 3),
+                           linewidth=6.0, opacity=args.opacity)
             actors.append(a)
 
     # cast as array
     voxels = np.asarray(voxels, dtype=float)
 
     cube_a = actor.cube(centers=voxels, colors=(1, 1, 1))
-    cube_a.GetProperty().SetOpacity(0.6)
+    cube_a.GetProperty().SetOpacity(0.8)
     actors.append(cube_a)
     order, full = get_sh_order_and_fullness(sh.shape[-1])
     sphere = get_sphere('symmetric724')
-    B_mat = sh_to_sf_matrix(sphere,
-                            sh_order=order,
+    B_mat = sh_to_sf_matrix(sphere, sh_order=order,
                             full_basis=full,
                             return_inv=False)
 
-    odf_a = actor.odf_slicer(sh, sphere=sphere, B_matrix=B_mat, opacity=1.0)
+    odf_a = actor.odf_slicer(sh, sphere=sphere, B_matrix=B_mat, opacity=0.6)
     actors.append(odf_a)
 
     scene = window.Scene()
     scene.add(*actors)
     if args.output:
         # scene.reset_camera()
-        snapshot(scene, args.output, size=(500, 500))
+        snapshot(scene, args.output, size=(1000, 1000))
     else:
         window.show(scene)
 
