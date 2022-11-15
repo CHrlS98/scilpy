3b3bfa14b1fed153dc53860a5122ec7f0f2a3cdc
Charles Poirier <charles.poirier@usherbrooke.ca>
2022-11-15 09:07:58 -0500

    Add majority vote for averaging

--- scripts/scil_average_registered_images.py
+++ scripts/scil_average_registered_images.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding:uft-8 -*-
 """
-Given a bunch of registered nifti images with corresponding x, y, z dimensions,
-compute an average map. The number of dimensions for the last dimension is
-given by the maximum number of values across all images.
+Given a bunch of registered nifti images with corresponding dimensions,
+compute an average map.
 """
 import argparse
 import nibabel as nib
 import numpy as np
 
-from scilpy.io.utils import assert_inputs_exist, assert_outputs_exist
+from scilpy.io.utils import assert_inputs_exist, assert_outputs_exist, add_overwrite_arg
 
 
 def _build_arg_parser():
@@ -18,7 +17,53 @@ def _build_arg_parser():
     p.add_argument('in_volumes', nargs='+', help='Input images to average.')
     p.add_argument('--out', help='Output image.', required=True)
 
+    p.add_argument('--summarize_metric', choices=['mean', 'vote'],
+                   default='mean', help='Metric used to summarize values.')
+
+    add_overwrite_arg(p)
     return p
+
+
+def compute_mean_image(args):
+    # first image
+    img = nib.load(args.in_volumes[0])
+    sum_of_vols = img.get_fdata()
+
+    for fname in args.in_volumes[1:]:
+        img = nib.load(fname)
+        img_data = img.get_fdata()
+        if img_data.shape[:3] != sum_of_vols.shape[:3]:
+            raise ValueError('Dimension mismatch for image {}'.format(fname))
+        sum_of_vols += img_data
+
+    sum_of_vols /= len(args.in_volumes)
+    return sum_of_vols
+
+
+def classify_majority_vote(args):
+    img = nib.load(args.in_volumes[0])
+    data = img.get_fdata().astype(int)
+    vshape = data.shape[:3]
+
+    # flat histogram
+    bins = np.zeros(np.append(data.shape, 11), dtype=int)
+    bins = np.reshape(bins, (-1, 11))
+
+    # process 1st image
+    bins[np.arange(len(bins)), data.flatten()] += 1
+
+    for fname in args.in_volumes[1:]:
+        img = nib.load(fname)
+        data = img.get_fdata().astype(int)
+        if data.shape[:3] != vshape:
+            raise ValueError('Dimension mismatch for image {}'.format(fname))
+        bins[np.arange(len(bins)), data.flatten()] += 1
+
+    # reshape image
+    bins = bins.reshape(np.append(data.shape, 11))
+
+    voted = np.argmax(bins, axis=-1)
+    return voted
 
 
 def main():
@@ -27,23 +72,15 @@ def main():
     assert_inputs_exist(parser, args.in_volumes)
     assert_outputs_exist(parser, args, args.out)
 
+    ref_im = nib.load(args.in_volumes[0])
+
     # first image
-    img = nib.load(args.in_volumes[0])
-    sum_of_vols = img.get_fdata(dtype=np.float32)[..., :10]
+    if args.summarize_metric == 'mean':
+        output = compute_mean_image(args)
+    else:
+        output = classify_majority_vote(args)
 
-    for fname in args.in_volumes[1:]:
-        img = nib.load(fname)
-        img_data = img.get_fdata(dtype=np.float32)[..., :10]
-        if img_data.shape[:3] != sum_of_vols.shape[:3]:
-            raise ValueError('Dimension mismatch for image {}'.format(fname))
-        if img_data.shape[-1] > sum_of_vols.shape[-1]:
-            img_data[..., :sum_of_vols.shape[-1]] += sum_of_vols
-            sum_of_vols = img_data
-        else:  # sum_of_vols.shape[-1] >= img.shape[-1]
-            sum_of_vols[..., :img_data.shape[-1]] += img_data
-
-    sum_of_vols /= len(args.in_volumes)
-    nib.save(nib.Nifti1Image(sum_of_vols, img.affine), args.out)
+    nib.save(nib.Nifti1Image(output.astype(ref_im.get_data_dtype()), ref_im.affine), args.out)
 
 
 if __name__ == '__main__':
